8ec3b2d3a1fd3aa7901c76fd22649e5ff8cdd72f
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-06 12:47:08 +0200

    creating a button to add raandom student and keep it in model context

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -4,20 +4,37 @@
 //
 //  Created by Ihor Sukhachov on 06.12.2025.
 //
-
+import SwiftData
 import SwiftUI
 
 
 
 struct ContentView: View {
-    
+    @Environment(\.modelContext) var modelContext
+    @Query var students: [Student]
     
     var body: some View {
-        
+        NavigationStack {
+             List(students) { student in
+                 Text(student.name)
+                }
+             .navigationTitle("Students")
+             .toolbar {
+                 Button("Add") {
+                     let firstNames = ["Ginny", "Ron", "Harry", "Hermiona "]
+                     let lastNames = ["Weasley", "Riddle ", "Potter", "Granger"]
+                     let chosenFirstName = firstNames.randomElement()!
+                     let chosenLastName = lastNames.randomElement()!
+                     
+                     let student = Student(id: UUID(), name: "\(chosenFirstName) \(chosenLastName)")
+                     modelContext.insert(student)
+                 }
+             }
+            }
+        }
     }
     
-    
-}
+ 
 
 #Preview {
     ContentView()
