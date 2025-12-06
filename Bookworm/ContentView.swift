710b1cbd28dcaf1cf7d8156fb4a46a7a9a2677b2
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-06 11:48:42 +0200

    creating a pushbutton view

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -7,13 +7,32 @@
 
 import SwiftUI
 
+struct PushButton: View {
+    let title: String
+    @State var isOm: Bool
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+    
+    
+    var body: some View {
+        Button(title) {
+            isOm.toggle()
+        }
+        .padding()
+        .background(LinearGradient (colors: isOm ? onColors : offColors, startPoint: .top, endPoint: .bottom))
+        .foregroundStyle(.white)
+        .clipShape(.capsule)
+        .shadow(radius: isOm ? 0 : 5)
+    }
+}
+
 struct ContentView: View {
+    
+    @State private var rememberMe = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Toggle("Remember me", isOn: $rememberMe)
         }
         .padding()
     }
