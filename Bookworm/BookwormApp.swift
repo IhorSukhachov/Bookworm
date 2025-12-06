756771d64047cd0dea1477624dd98ccf77f237c9
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-06 11:35:59 +0200

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Ihor Sukhachov on 06.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
