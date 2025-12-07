0cdf085761bf9db55b9972c651000c45c25a1e03
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-07 18:32:55 +0200

    creating add book interface

--- Bookworm/AddBookView.swift
+++ Bookworm/AddBookView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Ihor Sukhachov on 07.12.2025.
 //
-
+import SwiftData
 import SwiftUI
 
 struct AddBookView: View {
@@ -19,7 +19,36 @@ struct AddBookView: View {
     let genres: [String] = ["Fantasy", "Science Fiction", "Romance", "Mystery", "Horror"]
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+             Form {
+                 Section {
+                     TextField("Name of book", text: $title)
+                     TextField("Author's name", text: $author)
+                     
+                     Picker("Genre", selection: $genre) {
+                         ForEach(genres, id: \.self) {
+                             Text($0)
+                         }
+                     }
+                 }
+                 Section("Write a review") {
+                     TextEditor(text: $review)
+                     
+                     Picker("Rating", selection: $rating) {
+                         ForEach(0..<6) {
+                              Text(String($0))
+                         }
+                     }
+                 }
+                 
+                 Section {
+                     Button("Save") {
+                         let newBook = Book(title: title, author: author, genre: genre, review: review, rating: rating)
+                         modelContext.insert(newBook)
+                     }
+                 }
+             }.navigationTitle("Add Book")
+        }
     }
 }
 
