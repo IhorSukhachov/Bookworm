c0e93f71b3855db74b781870adb699fd461f2ca5
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-07 18:09:50 +0200

    creating the book class

--- Bookworm/Book.swift
+++ Bookworm/Book.swift
@@ -4,5 +4,22 @@
 //
 //  Created by Ihor Sukhachov on 07.12.2025.
 //
-
+import SwiftData
 import Foundation
+
+@Model
+class Book {
+    var title: String
+    var author: String
+    var genre: String
+    var review: String
+    var rating: Int
+    
+    init(title: String, author: String, genre: String, review: String, rating: Int) {
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.review = review
+        self.rating = rating 
+    }
+}
