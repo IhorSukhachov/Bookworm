029089b42b8f3956240d9c8d784827a7765b332e
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-07 18:42:16 +0200

    adding main page

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -5,15 +5,32 @@
 //  Created by Ihor Sukhachov on 06.12.2025.
 //
 
+import SwiftData
 import SwiftUI
 
 
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var books: [Book]
     
+    @State private var showingAddScreen: Bool = false
     
     var body: some View {
-        
+        NavigationStack {
+            Text("Count is \(books.count)")
+                .navigationTitle("Bookworm")
+                .toolbar {
+                    ToolbarItem(placement:.topBarTrailing) {
+                        Button("Add book", systemImage: "plus") {
+                            showingAddScreen.toggle()
+                        }
+                    }
+                    
+                }.sheet(isPresented: $showingAddScreen) {
+                    AddBookView()  
+                }
+        }
     }
 }
  
