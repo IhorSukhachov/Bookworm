2462ba8610bc614d655a3eb20c51622dcc1e962b
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-07 19:51:01 +0200

    new view

---
+++ Bookworm/EmojiRatingView.swift
@@ -0,0 +1,18 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Ihor Sukhachov on 07.12.2025.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmojiRatingView()
+}
