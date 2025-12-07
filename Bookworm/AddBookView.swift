e03931a5c79a6455d8769bcb5d09fc676c8cc508
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-07 18:19:20 +0200

    creating vars for class properties

--- Bookworm/AddBookView.swift
+++ Bookworm/AddBookView.swift
@@ -8,6 +8,16 @@
 import SwiftUI
 
 struct AddBookView: View {
+    @Environment(\.modelContext) var modelContext
+    
+    @State private var title: String = ""
+    @State private var author: String = ""
+    @State private var genre: String = "Fantasy"
+    @State private var review: String = ""
+    @State private var rating: Int = 3
+    
+    let genres: [String] = ["Fantasy", "Science Fiction", "Romance", "Mystery", "Horror"]
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
