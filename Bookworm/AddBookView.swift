f4c47c00c9984f9950058a0de5e91dcf5b1fc28c
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-07 18:14:25 +0200

    adding new view

---
+++ Bookworm/AddBookView.swift
@@ -0,0 +1,18 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Ihor Sukhachov on 07.12.2025.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddBookView()
+}
