15352b6d8b825cec2dc757feba795098c50cb4ee
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-07 19:25:51 +0200

    new rating view file

---
+++ Bookworm/RatingView.swift
@@ -0,0 +1,18 @@
+//
+//  RatingView.swift
+//  Bookworm
+//
+//  Created by Ihor Sukhachov on 07.12.2025.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingView()
+}
