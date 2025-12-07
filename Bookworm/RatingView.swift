9af83f08cee855399534de5a74aca3d408e233fe
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-07 19:30:49 +0200

    creating vars and binding

--- Bookworm/RatingView.swift
+++ Bookworm/RatingView.swift
@@ -8,11 +8,21 @@
 import SwiftUI
 
 struct RatingView: View {
+    @Binding var rating: Int
+    var label = ""
+    var maximumRating = 5
+    
+    var offImage: Image?
+    var onImage = Image(systemName: "star.fill")
+    
+    var offColor = Color.gray
+    var onColor = Color.yellow
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    RatingView()
+    RatingView(rating: .constant(4))
 }
