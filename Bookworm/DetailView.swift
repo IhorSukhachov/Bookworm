2abb890b65b5d26463fff9fd21aa7dba77654f4a
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-08 16:46:32 +0200

    creating a test book

--- Bookworm/DetailView.swift
+++ Bookworm/DetailView.swift
@@ -4,15 +4,25 @@
 //
 //  Created by Ihor Sukhachov on 08.12.2025.
 //
-
+import SwiftData
 import SwiftUI
 
 struct DetailView: View {
+    let book: Book
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    DetailView()
+    do {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: Book.self, configurations: config)
+        let example = Book(title: "Test Book", author: "Test Author", genre: "Test genre", review: "some review", rating: 3)
+        return DetailView(book: example)
+            .modelContainer(container)
+    } catch {
+        return Text("Failed to create book preview: \(error.localizedDescription)")
+    }
+   
 }
